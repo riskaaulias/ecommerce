aef57068f0ca9714ed1a3fb7a277f0c02dda4840
riskaaulias <riskasafirasafira954@gmail.com>
2025-12-16 11:43:44 -0800

    ngerjain database, desaign, dan migration

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,6 @@ Route::get('/', function () {
 Route::get('/tentang', function () {
      return view('tentang');
 });
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
